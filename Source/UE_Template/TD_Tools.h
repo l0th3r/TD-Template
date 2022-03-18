befa1c6b0b76e45f850430b3566a64f28000dbf3
Cosmo <58365407+l0th3r@users.noreply.github.com>
2022-03-18 11:59:10 +0100

    Added Tools cpp class

---
+++ Source/UE_Template/TD_Tools.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TD_Tools.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_TEMPLATE_API UTD_Tools : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
