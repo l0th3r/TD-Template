00ff466aff60008eb6446c3c4fe2bdeeb63292c3
Cosmo <58365407+l0th3r@users.noreply.github.com>
2022-03-28 15:10:45 +0100

    New Project to fix corrupted files

--- Source/TD_Template/TD_Template.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TD_Template.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TD_Template, "TD_Template" );
